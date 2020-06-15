df414989001331d2680e883ce041d5b9bf6d74b2
LTurns <lizzieturney@gmail.com>
2020-06-15 19:10:44 +0100

    small refactoring

--- lib/account.rb
+++ lib/account.rb
@@ -1,19 +1,17 @@
 class Account
 
-  def deposit(float)
+  def initialize
+    @time = Time.now.strftime("%d/%m/%Y")
     @deposit = []
-    time = Time.now
-    @deposit.push(time.strftime("%d/%m/%Y"), float, nil, float)
-    # we know that credit relates to deposit and withdrawal relates to debit so can start mirroring the table - the two floats should never class if we put
-    # them at different indexes.
-    # when the basics are done, ensure the correct amount of decimal places (2) are used - refactor.
+    @withdrawal = []
+  end
+
+  def deposit(float)
+    @deposit.push(@time, float, nil, float)
   end
 
   def withdraw(float)
-    time = Time.now
-    @withdrawal = []
-    @withdrawal.push(time.strftime("%d/%m/%Y"), nil, float, float)
-    # information we receive here will then mirror the print statement - so we simply need to concact all the results.
+    @withdrawal.push(@time, nil, float, float)
   end
 
   def print_statement
