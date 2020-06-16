249e44e3a94fed231b6579811aa747516f79a3f5
LTurns <lizzieturney@gmail.com>
2020-06-16 18:24:03 +0100

    added rubocop and simplecov

--- lib/account.rb
+++ lib/account.rb
@@ -1,5 +1,6 @@
-class Account
+# frozen_string_literal: true
 
+class Account
   attr_reader :balance
 
   def initialize(balance = 0.00)
@@ -7,18 +8,27 @@ class Account
   end
 
   def transaction(deposit = 0.00, withdrawal = 0.00)
-    @time = Time.now.strftime("%d/%m/%Y")
+    @time = Time.now.strftime('%d/%m/%Y')
     @deposit = deposit
     @withdrawal = withdrawal
     @balance += (@deposit - @withdrawal)
-    "credit: #{sprintf("%.2f", @deposit)}, debit: #{sprintf("%.2f", @withdrawal)}"
+    "credit: #{format('%.2f', @deposit)}, debit: #{format('%.2f', @withdrawal)}"
   end
 
   def print_statement
-    @result = ["date || credit || debit || balance"]
-    return @result.insert(1, ["#{@time} || #{@deposit = nil} || #{@withdrawal = nil} || #{sprintf("%.2f", @balance)}"]).join(" \n") if @deposit == 0.00 && @withdrawal == 0.00
-    return @result.insert(1, ["#{@time} || #{@deposit = nil} || #{sprintf("%.2f", @withdrawal)} || #{sprintf("%.2f", @balance)}"]).join(" \n") if @deposit == 0.00
-    return @result.insert(1, ["#{@time} || #{sprintf("%.2f", @deposit)} || #{@withdrawal = nil} || #{sprintf("%.2f", @balance)}"]).join(" \n") if @withdrawal == 0.00
-    return @result.insert(1, ["#{@time} || #{sprintf("%.2f", @deposit)} || #{sprintf("%.2f", @withdrawal)} || #{sprintf("%.2f", @balance)}"]).join(" \n ") if @deposit != 0.00 && @withdrawal != 0.00
+    @result = ['date || credit || debit || balance']
+    if @deposit == 0.00 && @withdrawal == 0.00
+      return @result.insert(1, ["#{@time} || #{@deposit = nil} || #{@withdrawal = nil} || #{format('%.2f', @balance)}"]).join(" \n")
+    end
+    if @deposit == 0.00
+      return @result.insert(1, ["#{@time} || #{@deposit = nil} || #{format('%.2f', @withdrawal)} || #{format('%.2f', @balance)}"]).join(" \n")
+    end
+    if @withdrawal == 0.00
+      return @result.insert(1, ["#{@time} || #{format('%.2f', @deposit)} || #{@withdrawal = nil} || #{format('%.2f', @balance)}"]).join(" \n")
+    end
+
+    if @deposit != 0.00 && @withdrawal != 0.00
+      @result.insert(1, ["#{@time} || #{format('%.2f', @deposit)} || #{format('%.2f', @withdrawal)} || #{format('%.2f', @balance)}"]).join(" \n ")
+    end
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,12 +1,12 @@
+# frozen_string_literal: true
+
 require 'account'
 
 RSpec.describe Account do
-
   context 'balance is initialized at 0 on object creation' do
-
     before(:each) do
-       @account = Account.new
-     end
+      @account = Account.new
+    end
 
     it 'shows balance to be equal to 0.00' do
       expect(@account.balance).to eq(0.00)
@@ -15,29 +15,27 @@ RSpec.describe Account do
     it 'displays balance as a type of float' do
       expect(@account.balance).to be_a_kind_of(Float)
     end
-
   end
 
   describe '#transaction' do
-
     before(:each) do
-       @account = Account.new
-     end
+      @account = Account.new
+    end
 
     it 'shows the debit and credit amount entered by the user' do
-     expect(@account.transaction).to eq("credit: 0.00, debit: 0.00")
+      expect(@account.transaction).to eq('credit: 0.00, debit: 0.00')
     end
 
     it 'has a default value of zero for both credit and debit amount' do
-      expect(@account.transaction).to eq("credit: 0.00, debit: 0.00")
+      expect(@account.transaction).to eq('credit: 0.00, debit: 0.00')
     end
 
     it 'maintains the default value of zero for withdrawal when deposit amount is altered' do
-      expect(@account.transaction(100)).to eq("credit: 100.00, debit: 0.00")
+      expect(@account.transaction(100)).to eq('credit: 100.00, debit: 0.00')
     end
 
     it 'maintains the default value of zero for deposit when withdrawal amount is altered' do
-      expect(@account.transaction(0, 100)).to eq("credit: 0.00, debit: 100.00")
+      expect(@account.transaction(0, 100)).to eq('credit: 0.00, debit: 100.00')
     end
 
     it 'displays the credit and debit amounts in a string' do
@@ -45,26 +43,25 @@ RSpec.describe Account do
     end
 
     it 'adds the user transaction to the balance' do
-      @account.transaction(100,50)
+      @account.transaction(100, 50)
       expect(@account.balance).to eq(50.00)
     end
 
     it 'enabes the balance to be "in dept" with minus numbers' do
-      @account.transaction(50,100)
+      @account.transaction(50, 100)
       expect(@account.balance).to eq(-50.00)
     end
   end
 
-    describe '#print_statment' do
-
-      before(:each) do
-         @account = Account.new
-         @time = Time.now.strftime("%d/%m/%Y")
-       end
+  describe '#print_statment' do
+    before(:each) do
+      @account = Account.new
+      @time = Time.now.strftime('%d/%m/%Y')
+    end
 
     it 'returns the bank statement as a string' do
-        @account.transaction
-        expect(@account.print_statement).to eq("date || credit || debit || balance \n#{@time} ||  ||  || 0.00")
+      @account.transaction
+      expect(@account.print_statement).to eq("date || credit || debit || balance \n#{@time} ||  ||  || 0.00")
     end
 
     it 'returns a blank withdrawal column when only a deposit is made' do
